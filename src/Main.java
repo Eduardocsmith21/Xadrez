e6690d4722d32372b3a0fa36f7f8b4c77db2b7a6
Eduardo Carvalho Smith <eduardosmith@sad.infovia-mt>
2024-08-19 14:06:12 -0400

    Criação classe Position

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import bordGame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste");
+
+        Position position = new Position(3, 5);
+
+        System.out.println(position);
     }
 }
